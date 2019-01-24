1b143968106a74b4845b1877b4f2d58e15dafe6a
Ruslan <ruslanbegun@gmail.com>
2019-01-24 02:25:11 +0200

    'routing'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {LogService} from '@services/log.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   providers: [LogService]
 })
 export class AppComponent implements AfterViewInit {
